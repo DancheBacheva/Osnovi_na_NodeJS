6e7d855d58e1f6ca1e75548e60f01b5dbea8b5cf
DancheBacheva <danchebacheva@gmail.com>
2023-07-01 23:27:55 +0200

    popraveni greski

--- Domasna od cas8/app.js
+++ Domasna od cas8/app.js
@@ -4,12 +4,8 @@ const kalkulator = require("./controller/kalkulator");
 const app = express();
 
 app.get("/plostinaP/:a/:b", kalkulator.plostinaPravoagolnik);
-app.get("/vrednost/:c2f", kalkulator.pretvoriStepeni);
-app.get("/vrednost/:f2c", kalkulator.pretvoriStepeni);
-app.get("/operacija/:sobiranje/:a/:b", kalkulator.kalkulator);
-app.get("/operacija/:odzemanje/:a/:b", kalkulator.kalkulator);
-app.get("/operacija/:delenje/:a/:b", kalkulator.kalkulator);
-app.get("/operacija/:mnozenje/:a/:b", kalkulator.kalkulator);
+app.get("/pretvoriStepeni/:operacija/:broj", kalkulator.pretvoriStepeni);
+app.get("/kalkulator/:op/:a/:b", kalkulator.kalkulator);
 app.get("/newton/:mass/:acc", kalkulator.presmetajSila);
 app.get("/plostinaT/:a/:b", kalkulator.plostinaTriagolnik);
 
--- Domasna od cas8/controller/kalkulator.js
+++ Domasna od cas8/controller/kalkulator.js
@@ -23,25 +23,30 @@ const plostinaPravoagolnik = async (req, res) =>{
  const b = req.params.b;
  const plostinaP = a*b;
  const response = await parseTemplate("rezultat.html", {
-  ime: "Pravoagolnik",
-  data: plostinaP
+  ime: "Плоштината на правоаголник",
+  data: plostinaP,
+  opis: "Формулата за пресметување плоштина на правоаголник е: P=a*b"
 });
 res.send(response);
 };
 
 //? 2. DA SE PRESMETA F2C i obratno DA SE IMPLEMENTIRA SWITCGH
 const pretvoriStepeni = async (req,res)=>{
-  switch(req.params.vrednost){
+  let rezultat = "";
+  switch(req.params.operacija){
     case "c2f":
-      return res.send((vrednost*9/5)+32);
+      rezultat = `${Number(req.params.broj*9/5)+32}`
+      break;
     case "f2c":
-      return res.send((vrednost-32)*5/9);
+      rezultat = `${Number(req.params.broj-32)*5/9}`
+      break;
     default:
-      return res.send("Error!"); 
+      rezultat = "Nepoznat operator"; 
   }
   const response = await parseTemplate("rezultat.html", {
-    ime: "stepeni",
-    data: vrednost
+    ime: "Претворање степени",
+    data: rezultat,
+    opis: "Овде може да претворите степени од °C во °F и обратно"
   })
   res.send(response);
 };
@@ -49,21 +54,27 @@ const pretvoriStepeni = async (req,res)=>{
 
 //? 3. DA SE KREKIRA KALKULATOR(SWITH || IF ELSE)
 const kalkulator = async (req, res) => {
-  switch(req.params.operacija){
+  let rezultat = "";
+  switch(req.params.op){
     case "sobiranje":
-    return res.send(`${Number(req.params.a) + Number(req.params.b)}`);
+    rezultat = `${Number(req.params.a) + Number(req.params.b)}`;
+    break;
   case "odzemanje":
-    return res.send(`${Number(req.params.a) - Number(req.params.b)}`);
+    rezultat = `${Number(req.params.a) - Number(req.params.b)}`;
+    break;
   case "delenje":
-    return res.send(`${Number(req.params.a) / Number(req.params.b)}`);
+    rezultat = `${Number(req.params.a) / Number(req.params.b)}`;
+    break;
   case "mnozenje":
-    return res.send(`${Number(req.params.a) * Number(req.params.b)}`);
+    rezultat = `${Number(req.params.a) * Number(req.params.b)}`;
+    break;
   default:
     return res.send("Nepoznat operator!");
   }
   const response = await parseTemplate("rezultat.html", {
-    ime: "kalkulator",
-    data: operacija
+    ime: "Калкулатор",
+    data: rezultat,
+    opis: "Внесете операција и број и пресметајте го збирот, разликата, производот или количникот на два броја."
   })
   res.send(response);
 };
@@ -76,8 +87,9 @@ const presmetajSila = async (req, res) => {
   const acc = req.params.acc;
   const newton = mass * acc;
   const response = await parseTemplate("rezultat.html", {
-    ime: "newton",
-    data: newton
+    ime: "Сила",
+    data: newton,
+    opis: "Формулата за пресметување сила е: F=m*a"
   })
   res.send(response);
 };
@@ -91,6 +103,7 @@ const plostinaTriagolnik = async (req, res) =>{
   const response = await parseTemplate("rezultat.html", {
     ime: "triagolnik",
     data: plostinaT,
+    opis: "Формулата за пресметување плоштина на триаголник е: P=a*b/2"
   });
  res.send(response);
  };
@@ -104,4 +117,6 @@ kalkulator,
 presmetajSila,
 plostinaTriagolnik,
 };
+
+
 //? SITE OVIE FUNKCII DA VRAKJAAT WEB STRANIICA
